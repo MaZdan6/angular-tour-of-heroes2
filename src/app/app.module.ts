1391d55f2ac3ca33fc51f29d231b75880668c474
MaZdan6 <fazol66@gmail.com>
2017-04-25 23:15:03 +0200

    6.1 Splitting the AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {HeroesComponent} from './heroes.component';
+import {AppComponent} from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
